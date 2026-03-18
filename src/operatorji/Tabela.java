627170bca085fae3adbd08ab7010e170e63d3621
Tomaz <dobravec@gmail.com>
2026-03-18 16:51:47 +0100

    p5

---
+++ src/operatorji/Tabela.java
@@ -0,0 +1,13 @@
+package operatorji;
+
+public class Tabela {
+
+  public static void main(String[] args) {
+    char[] tabela = new char[25];
+
+    char[][] zaslon = new char[25][80];
+
+    int steviloVrstic = zaslon.length;
+    int steviloStolpcevPrveVrstice = zaslon[0].length;
+  }
+}
